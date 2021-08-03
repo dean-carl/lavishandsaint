30012713c163ec284964498ab2e9bce99484a597
dean-carl <46971242+dean-carl@users.noreply.github.com>
2021-08-03 15:51:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// menu control ----------------
+
+const openBar = document.querySelector(".toggle");
+const container = document.querySelector('.container');
+const menu = document.querySelector(".menu");
+
+
+openBar.addEventListener("click", function () {
+
+
+    openBar.classList.toggle('active');
+    container.classList.toggle('active');
+
+})
+
+menu.addEventListener("click", function () {
+    container.classList.toggle('active')
+    openBar.classList.toggle('active');
+})
+
+
+// movie control---------------- 
+
+//--call movies here -------
+
+movieTitle = ["beach", "womanSea", "Dubai", "PalmTreesFly"]
+
+
+// -------Play random movie----------
+
+
+function loadMovie() {
+
+    let movieNumber = Math.floor(Math.random() * movieTitle.length)
+
+    let movieLocation = `./images/${movieTitle[movieNumber]}.mp4`;
+
+
+    document.getElementById("movie").src = movieLocation
+    console.log(movieLocation)
+}
+
+// setInterval(loadMovie, 8000)
+
+//------Play movies in order------------
+
+
+// function loadMovieLoop() {
+
+
+//     for (let n = 0; n < movieTitle.length; n++) {
+
+//         playHead(n)
+//     }
+
+//     function playHead(n) {
+
+//         setTimeout(() => {
+//             document.getElementById("movie").setAttribute('src', `./images/${movieTitle[n]}.mp4`)
+//         }, 5000 * n);
+
+//     }
+// }
+
+// setInterval(loadMovieLoop, 5000 * movieTitle.length)
+
